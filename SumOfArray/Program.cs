4833a89eb5d2dd459a9fc436b5406c9e2a81f98f
Vinay Gajanan Tekkur <vinaygt@gisi.com>
2019-06-27 23:52:39 +0530

    Sample Sum of Array program

---
+++ SumOfArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfArray
+{
+    class Program
+    {
+        public static void SumOfArray(int[] arr)
+        {
+            int sum = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                sum = sum + arr[i];
+            }
+            Console.WriteLine($"Sum of the array is : {sum}");
+        }
+
+        public static void SqrtOfArray(int[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                double sq = Math.Sqrt(arr[i]);
+                Console.WriteLine($"Square Root of each elements of array is : {sq}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the value of n :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter Elements one by one :");
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            SumOfArray(arr);
+            SqrtOfArray(arr);
+        }
+    }
+}
